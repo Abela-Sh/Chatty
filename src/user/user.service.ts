0e2f2191b4242f4a97795cbb085bc626e77784a4
abela-sh <abelatshege@gmail.com>
2024-05-18 09:47:19 -0700

    Second Assignment

---
+++ src/user/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+    getData(){
+        const user = {
+            id:1,
+            name: 'Joe',
+            email: 'joe@gmail.com'
+        }
+        return user
+    }
+}
