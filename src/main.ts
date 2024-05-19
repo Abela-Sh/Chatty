3337d7ecea7b4929b36cad300ee005534a3c15f1
abela-sh <abelatshege@gmail.com>
2024-05-19 00:44:51 -0700

    Assignment 5 Completed.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerMiddleware } from './user/Midddleware/logging.middleware';
 import { AuthGuard } from './user/Guards/auth.guard';
+import { AllExceptionsFilter } from './user/Exceptions/exception.handler';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(new LoggerMiddleware().use)
   app.useGlobalGuards(new AuthGuard());
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3000);
 }
 bootstrap();
